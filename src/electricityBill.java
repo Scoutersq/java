1df6ee2a22537ef7d3a396bd39369640e845780d
Aditya Raj <adityarajp1208@gmail.com>
2025-11-18 11:38:35 +0530

    Electricity bill generator

---
+++ src/electricityBill.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class electricityBill {
+    public static void main(String[] args) {
+        System.out.println("Enter the unit of electric bill:");
+        Scanner sc = new Scanner(System.in);
+
+        int unit = sc.nextInt();
+        int bill;
+
+        if (unit <= 100){
+            bill = unit * 5;
+            System.out.println("Bill of" +  " unit "  +  unit +  " is "  + bill);
+        }
+        else if (unit > 100 && unit <= 200){
+            bill = (100 * 5) + (unit - 100) * 7;
+            System.out.println("Bill of" +  " unit "  +  unit +  " is "  +  bill);
+        }
+        else if (unit > 200 && unit <= 300) {
+            bill = (100 * 5) + (100 * 7) + (unit - 200) * 10;
+            System.out.println("Bill of" +  "  unit "  +  unit +  " is "  + bill);
+        }
+        else if (unit > 300) {
+            bill = (100 * 5) + (100 * 7) + (100 * 10) + (unit - 300) * 12;
+            System.out.println("Bill of" +  " unit "  +  unit +  " is "  +  bill);
+        }
+        else {
+            System.out.println("Invalid unit");
+        }
+    }
+}
